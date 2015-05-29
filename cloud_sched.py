520a4d814afbec72b36675c4b128b11c6e8b0440
Thiago Kenji Okada <thiago.mast3r@gmail.com>
2015-05-29 15:54:13 -0300

    Implement LTF

--- cloud_sched.py
+++ cloud_sched.py
@@ -10,7 +10,7 @@ from matplotlib import pyplot
 from swf_parser import parse_swf_file
 
 
-def filter_tasks(tasks, task_limit=500, time_limit=300, status_code=None, shuffle=None):
+def filter_tasks(self, task_limit=500, time_limit=300, status_code=None, shuffle=None):
     count = 0
     filtered_tasks = []
     if shuffle:
@@ -45,7 +45,7 @@ def tasks_statistics(tasks, field):
     for task in tasks:
         count_data += 1
         data.append(task[field])
-        
+
     sum_data = sum(data)
     mean_data = numpy.mean(data)
     stdev_data = numpy.std(data)
@@ -54,6 +54,15 @@ def tasks_statistics(tasks, field):
             "sum": sum_data,
             "mean": mean_data,
             "stdev": stdev_data}
+
+
+def print_tasks(tasks, limit=None):
+    count = 0
+    for task in tasks:
+        if limit and count >= limit:
+            break
+        pprint(task)
+        count += 1
 
 
 def tasks_histogram(tasks, field, bins=10):
@@ -68,10 +77,28 @@ def tasks_histogram(tasks, field, bins=10):
     pyplot.show()
 
 
+def largest_task_first(tasks, max_n_procs):
+    for procs in range(1, max_n_procs + 1):
+        pre_processed_tasks = pre_process_tasks_alg(tasks, procs)
+        processed_tasks = sorted(pre_processed_tasks, key=lambda t:
+                                 t['run_time'] *
+                                 t['number_of_allocated_processors'],
+                                 reverse=True)
+        #pprint(processed_tasks)
+        #for task in processed_tasks:
+            #print("Task {} total run time: {}"
+                  #.format(task['job_number'],
+                          #task['run_time'] *
+                          #task['number_of_allocated_processors']))
+        return tasks
+
+
 if __name__ == "__main__":
     tasks = parse_swf_file("UniLu-Gaia-2014-2.swf")
-    for i in range(3):
-        filtered_tasks = filter_tasks(tasks, 500, 300, 1, i)
-        processed_tasks = pre_process_tasks_alg(filtered_tasks, 2)
-        pprint(tasks_statistics(processed_tasks, 'run_time'))
-        tasks_histogram(processed_tasks, 'run_time', 50)
+    filtered_tasks = filter_tasks(tasks, 500, 300, 1)
+    #processed_tasks = pre_process_tasks_alg(filtered_tasks, 2)
+    #print_tasks(processed_tasks, 10)
+    #stats = tasks_statistics(processed_tasks, 'run_time')
+    #pprint(stats)
+    #tasks_histogram(processed_tasks, 'run_time', 50)
+    largest_task_first(filtered_tasks, 1)
