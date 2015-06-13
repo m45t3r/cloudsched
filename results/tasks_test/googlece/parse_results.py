81a617cce9b93727b3a4558c681963eacb74b969
Thiago Kenji Okada <thiago.mast3r@gmail.com>
2015-06-13 14:23:03 -0300

    Fixing tests results

--- results/tasks_test/googlece/parse_results.py
+++ results/tasks_test/googlece/parse_results.py
@@ -12,7 +12,11 @@ with open("summary.csv", 'w') as csv_file:
     csv_writer.writeheader()
     for n, filename in enumerate(sorted(glob.glob("*.log")), 1):
         with open(filename, 'r') as log_file:
-            results = [float(x) for x in log_file]
+            results = []
+            for line_number, line in enumerate(log_file, 1):
+                if line_number > 10:
+                    break
+                results.append(float(x)
             mean = round(statistics.mean(results), ndigits=2)
             ci95 = round(0.95 * statistics.stdev(results), ndigits=2)
             csv_writer.writerow({"n": n, "mean": "{0:.2f}".format(mean), "ci95": "{0:.2f}".format(ci95)})
