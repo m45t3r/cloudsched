fc8e9cf930d5ea1e8689020d99f977b74a121af2
Thiago Kenji Okada <thiago.mast3r@gmail.com>
2015-06-15 20:10:57 -0300

    Updating plots

--- results/tasks_test/parse_results.py
+++ results/tasks_test/parse_results.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python2
 
+from __future__ import division
+
 import csv
 import glob
+import math
 import os
 import sys
 
 import numpy
 
-fieldnames = ["n", "mean", "ci95"]
+N=32
+fieldnames = ["n", "expected", "mean", "ci95"]
 try:
     folder = sys.argv[1]
 except IndexError:
     sys.exit("usage: {} RESULTS_FOLDER".format(sys.argv[0]))
+
+with open(os.path.join(folder, 'result32.log')) as base_file:
+    results = []
+    for line_number, line in enumerate(base_file, 1):
+        if line_number > 10:
+            break
+        results.append(float(line))
+    base_mean = round(numpy.mean(results), ndigits=2)
 
 with open("summary_{}.csv".format(folder), 'wb') as csv_file:
     csv_writer = csv.DictWriter(csv_file, fieldnames)
@@ -21,10 +33,12 @@ with open("summary_{}.csv".format(folder), 'wb') as csv_file:
     for n, filename in enumerate(sorted(glob.glob("*.log")), 1):
         with open(filename, 'r') as log_file:
             results = []
+            base = None
             for line_number, line in enumerate(log_file, 1):
                 if line_number > 10:
                     break
                 results.append(float(line))
-            mean = round(numpy.mean(results), ndigits=2)
-            ci95 = round(0.95 * numpy.std(results), ndigits=2)
-            csv_writer.writerow({"n": n, "mean": "{0:.2f}".format(mean), "ci95": "{0:.2f}".format(ci95)})
+            mean = numpy.mean(results)
+            stdev = numpy.std(results)
+            csv_writer.writerow({"n": n, "expected": "{0:.2f}".format(math.ceil(N/n) * base_mean),
+                "mean": "{0:.2f}".format(round(mean, 2)), "ci95": "{0:.2f}".format(1.96 * stdev/math.sqrt(len(results)))})
--- results/tasks_test/plot_graphics.gp
+++ results/tasks_test/plot_graphics.gp
@@ -2,13 +2,12 @@
 
 set encoding utf8
 
-set title "32 moldable tasks (1024x1024 matrix multiplication)"
+#set title "32 moldable tasks (1024x1024 matrix multiplication)"
 
 set xlabel "Number of VMs"
 set ylabel "Execution time (s)"
 
 set xrange [0:33]
-set yrange [0:500]
 
 #set yrange [8:500]
 #set logscale y
@@ -29,5 +28,10 @@ set term pdfcairo # better quality
 azure = "summary_azure.csv"
 googlece = "summary_googlece.csv"
 
-set output "summary.pdf"
-plot azure using 1:2 title "Microsoft Azure" with lines lc rgb 'blue', azure using 1:2:3 notitle with yerrorbars lc rgb 'black' pt 1 lw 2, googlece using 1:2 title "Google CE" with lines lc rgb 'green', googlece using 1:2:3 notitle with yerrorbars lc rgb 'black' pt 1 lw 2
+set output "summary_azure.pdf"
+set yrange [0:600]
+plot azure using 1:2 title "Expected Time (s)" with lines, azure using 1:3 title "Measured Time (s)" with lines, azure using 1:3:4 notitle with yerrorbars lc rgb 'black' pt 1 lw 2
+
+set yrange [0:450]
+set output "summary_googlece.pdf"
+plot googlece using 1:2 title "Expected Time (s)" with lines, googlece using 1:3 title "Measured Time (s)" with lines, googlece using 1:3:4 notitle with yerrorbars lc rgb 'black' pt 1 lw 2
