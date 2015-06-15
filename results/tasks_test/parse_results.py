d4a6e0534d40a16bd6f6714322df0ca46f1b8846
Thiago Kenji Okada <thiago.mast3r@gmail.com>
2015-06-15 09:20:45 -0300

    Using correct stdev

--- results/tasks_test/parse_results.py
+++ results/tasks_test/parse_results.py
@@ -5,7 +5,7 @@ import glob
 import os
 import sys
 
-from backports import statistics
+import numpy
 
 fieldnames = ["n", "mean", "ci95"]
 try:
@@ -13,7 +13,7 @@ try:
 except IndexError:
     sys.exit("usage: {} RESULTS_FOLDER".format(sys.argv[0]))
 
-with open("summary_{}.csv".format(folder), 'w') as csv_file:
+with open("summary_{}.csv".format(folder), 'wb') as csv_file:
     csv_writer = csv.DictWriter(csv_file, fieldnames)
     csv_writer.writeheader()
 
@@ -25,6 +25,6 @@ with open("summary_{}.csv".format(folder), 'w') as csv_file:
                 if line_number > 10:
                     break
                 results.append(float(line))
-            mean = round(statistics.mean(results), ndigits=2)
-            ci95 = round(0.95 * statistics.stdev(results), ndigits=2)
+            mean = round(numpy.mean(results), ndigits=2)
+            ci95 = round(0.95 * numpy.std(results), ndigits=2)
             csv_writer.writerow({"n": n, "mean": "{0:.2f}".format(mean), "ci95": "{0:.2f}".format(ci95)})
