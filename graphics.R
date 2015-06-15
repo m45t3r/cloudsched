310513b484c9ad59990af80d1bbafa4723333907
marcosamaris <marcos.amaris@gmail.com>
2015-06-15 00:31:44 -0300

    Added a litle script to generate plots and two images of the data

---
+++ graphics.R
@@ -0,0 +1,15 @@
+library(ggplot2)
+
+dirpath <- "~/Dropbox/Doctorate/Results/cloudsched/results/sched_test/processed"
+setwd(paste(dirpath, sep=""))
+
+google <- read.table(paste("./googlece_n32_temp.csv",sep=""),sep = ",",header = TRUE)
+azure <- read.table(paste("./azure_n32_temp.csv",sep=""),sep = ",",header = TRUE)
+
+ggplot(google, aes(x=Algorithms,y=Time, fill = Operation)) + 
+    geom_bar(stat="identity", position = "dodge",data = google) + 
+    scale_fill_brewer(palette = "Set1")
+
+ggplot(azure, aes(x=Algorithms,y=Time, fill = Operation)) + 
+    geom_bar(stat="identity", position = "dodge",data = azure) + 
+    scale_fill_brewer(palette = "Set1")
